e4c67d44bc671890d7a4e280b792128886afe204
Gokhan Agar <gokhanagar1@gmail.com>
2023-01-03 21:38:52 +0300

    V7

--- donguler/for.py
+++ donguler/for.py
@@ -8,25 +8,49 @@ names = ['cinar','canan','beyza','sena','suna','zahide']
 # for name in names:
 #     print(f'my name is {name}' )
 
+name = 'Gokhan agar'
+
+# for n in name:
+#     print(n)
+
+tuple = (1,2,3,4,5)
+tuple2 = [(1,2),(1,4),(2,5)]
 
 
+# for t in tuple:
+#     print(t)
+
+# for t in tuple2:
+#     print(t)
+
+d = {'k1':1,'k2':2,'k3':3}
+
+# for key,value in d.items():
+#     print(key,value)
 
 
+#==============================================================================================
 
+sayilar = [1,3,5,7,9,12,19,21]
 
+# Soru 1
+# for n in sayilar:
+#     if n %3 ==0:
+#         print(n)
 
+# Soru 2
+# toplam =0
 
+# for n in sayilar:
+#     toplam += n
 
+# print(toplam)
 
+# Soru 3
 
-
-
-
-
-
-
-
-
+# for n in sayilar:
+#     if n%3==0:
+#         print(n**2)
 
 
 
