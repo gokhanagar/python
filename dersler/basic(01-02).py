b7b1092b9a2a7ae68d0e2a76c8184de38b65ad56
gokhanagar <gokhanagar1@gmail.com>
2023-02-07 22:03:40 +0300

    V21

---
+++ dersler/basic(01-02).py
@@ -0,0 +1,91 @@
+# x = 5
+# # = ile atama yapılır. matematikteki eşitlik değildir. x variable na 5 değeri atanır. 
+
+# #print(x)
+# #print(x*6)
+
+# y="hello"
+
+# # string variable lar "" içinde yazılır
+# print(y)
+
+# print(x,y)
+
+# # print()
+
+# # bir fonksiyondur. () varsa fonksiyon olduğunu gösterir. içine parametreler alır. sep= ile parametreler arasında olmasını
+# # istediğmiz karakteri yazarız. default değeri boşluktur
+
+# print(x,y, sep="---")
+
+# input()
+# bu fonksiyon ile kullanıcıdan alınan bir değer bir variable olarak atanır
+
+# name = input("adiniz nedir: ")
+# print(name)
+
+# kullanıcıdan aldığımız yaş bilgisinin iki katını ekrana yazdıran bir program yazalım
+
+#yas = input("yasinizi giriniz: ")  # kullanıcıdan alınan değer string bir değer olduğu için yanyana yazdı 
+# print(yas * 2)
+#print(int(yas) * 2)
+
+
+# kullanıcıdan alınan iki sayının ortalamasını bulma
+
+# sayi1 = int(input("sayi 1 giriniz: "))
+# sayi2 = int(input("sayi 2 giriniz: "))
+# ortalama = (sayi1 + sayi2) /2
+# print(ortalama)
+
+
+answer = 0
+
+for i in range(2):
+    x = input("please enter a number")
+    answer += int(x)
+print(answer / 2)     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
